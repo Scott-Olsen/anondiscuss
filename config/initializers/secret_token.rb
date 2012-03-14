ef443014763fe12750b7cf30a45c669705b15f2c
Scott Olsen <solsen230@gmail.com>
2012-03-13 23:56:55 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anondiscuss::Application.config.secret_token = '13429ede409d56ba2d3abb2e4766f2e4b5642d6eb411b47985b11a9c163f71c1328e8c0aceab5cdd834877d28033118563157886f54d2af1a6012422221149e1'
