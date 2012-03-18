43983673939f73017c111d44c3b13fb8f863cd6f
Scott Olsen <solsen230@gmail.com>
2012-03-18 00:03:21 -0400

    Update rspec test file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,39 +2,24 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  subject { page }
+
   let(:base_title) { "Anondisuss" }
   describe "Home page" do
-    it "should have the h1 'Anondiscuss'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Anondiscuss')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Anondiscuss")
-    end
+    before { visit root_path }
+    it { should have_selector('h1',	text: 'Anondiscuss') }
+    it { should have_selector('title',	text: full_title('') )
   end
 
   describe "Help page" do
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "Anondiscuss | Help")
-    end
+    before { visit help_path }
+      it { should have_selector('h1', text: 'Help') }
+      it { should have_selector('title', text: full_title(''))
   end
 
   describe "About page" do
-    it "should have the h1 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About')
-    end
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "Anondiscuss | About")
-    end
+    before { visit about_path }
+      it { should have_selector('h1',	text: 'About')
+      it { should have_selector('title',	text: full_title('') )
   end
 end
