6100212c0bf60629cb29ad869a289499fc0b392b
Scott Olsen <scott.t.olsen@gmail.com>
2012-09-11 04:07:16 +0000

    Updated .gitignore and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Anondiscuss::Application.routes.draw do
   root :to => "static-pages#home"
 
   match '/help',	to: 'static_pages#help'
-  match "/about',	to: 'static_pages#about'
+  match '/about',	to: 'static_pages#about'
 
 end
