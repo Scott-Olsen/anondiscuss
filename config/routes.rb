b26ecbd047c0582b99ad64b218c02dff5e521d3e
Scott Olsen <scott.t.olsen@gmail.com>
2012-09-12 08:12:53 +0000

    Changed root route to posts index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Anondiscuss::Application.routes.draw do
     resources :comments
   end
 
-  root :to => "static-pages#home"
+  root :to => "posts#index"
 
   match '/help',	to: 'static_pages#help'
   match '/about',	to: 'static_pages#about'
