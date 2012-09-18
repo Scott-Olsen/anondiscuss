2342c5d6fee636b684ac6c6899561ee90bbb177f
Scott Olsen <scott.t.olsen@gmail.com>
2012-09-18 21:11:50 +0000

    added create to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,4 +6,13 @@ class CommentsController < ApplicationController
       format.html { redirect_to posts_url, notice: 'Comment invisible' }
     end
   end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params:comment)
+    respond_to do |format|
+      format.html { redirect_to post_path(@post), notice: 'Comment Posted' }
+      format.js
+    end
+  end
 end
